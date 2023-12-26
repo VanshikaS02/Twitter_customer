1944c3781a99731a2654201c2ebdcee8a5924ac5
VanshikaS02 <148053256+VanshikaS02@users.noreply.github.com>
2023-12-26 12:37:18 +0400

    Update clean_collected_data.py

--- clean_collected_data.py
+++ clean_collected_data.py
@@ -1,3 +1,5 @@
+# clean_collected_data.py
+
 import pandas as pd
 from textblob import TextBlob
 
@@ -15,28 +17,30 @@ def preprocess_data():
     try:
         data = pd.read_csv(r'C:\Users\vansh\Desktop\Twitter_customer\collected_data.csv')
         data = data.fillna('0')
-        float_list = ['comment_likes', 'comment_retweets', 'comment_replies']
+        float_list = ['comment_likes', 'comment_retweets', 'comment_replies', 'follower_count']
         for x in float_list:
             data[x] = convert_str(data[x])
         data = data[data['comment'].notna()]
         data['sentiments'] = generate_sentiments(data)
-        X = data['comment']
-        y = data['comment_likes']
-        z = data['sentiments']
-        small_data = pd.DataFrame(list(zip(X[:5], y[:5], z[:5])), columns=['Comment', 'Comment Likes', 'Comment Sentiment Score'])
-
+        data['user_engagement'] = data['comment_likes'] + data['comment_retweets'] + data['comment_replies']
+        data['average_engagement_per_follower'] = data['user_engagement'] / data['follower_count']
+        
+        # Calculate average reply depth
+        data['reply_depth'] = data['reply_to'].apply(lambda x: len(str(x).split(',')) if pd.notna(x) else 0)
+        average_reply_depth = data.groupby('username')['reply_depth'].mean().reset_index()
+        average_reply_depth.columns = ['username', 'average_reply_depth']
+        data = pd.merge(data, average_reply_depth, on='username', how='left')
+        
         # Add logging statements
         print("Data preprocessing successful.")
         
-        # Print the head of the small_data DataFrame
-        print("Small DataFrame head:")
-        print(small_data.head())
+        # Print the head of the DataFrame
+        print("DataFrame head:")
+        print(data.head())
 
-        return data, small_data
+        return data
     except Exception as e:
         print("Error during data preprocessing:", str(e))
-        return pd.DataFrame(), pd.DataFrame()
+        return pd.DataFrame()
 
-
-data, small_data = preprocess_data()
-print(data.head())
+data = preprocess_data()
